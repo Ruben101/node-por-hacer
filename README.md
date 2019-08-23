25d9b37159679d4b508de00a2038f90e162d04c4
Ruben <ruben.abellan@ext.leroymerlin.es>
2019-08-23 14:57:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Aplicación de comandos
+# Ejemplo ejecución:
+# node app crear -d "comer"
+# node app node app listar
+# node app actualizar -d "comer" -c true
+# node app borrar -d "comer"
+
+Este es el ejercicio del curso
+
+
+Recuerden instalar los paquetes de node
+
+```
+npm install
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+    });
+
+}
+
+const cargarDB = () => {
+
+    try {
+
+        listadoPorHacer = require('../db/data.json');
+
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+}
+
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+
+    /* Otra forma de hacerlo, obteniendo el objeto tarea */
+    /*let tarea = listadoPorHacer.find(tarea => tarea.descripcion === descripcion);
+
+    if (tarea != undefined) {
+        tarea.completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }*/
+}
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+
+    let nuevoListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+}
+
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
